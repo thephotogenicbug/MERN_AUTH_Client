c047877763d283e1ba61cbbdecb8f46f0745da9b
thephotogenicbug <navinkumarachari@gmail.com>
2025-06-24 13:19:10 +0530

    add user authentication middleware and update auth routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { login, logout, register } from "../controller/authController.js";
+import {
+  login,
+  logout,
+  register,
+  sendVerifyOtp,
+  verifyEmail,
+} from "../controller/authController.js";
+import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
 
@@ -9,5 +16,9 @@ authRouter.post("/register", register);
 authRouter.post("/login", login);
 // @ /api/auth/logout
 authRouter.post("/logout", logout);
+// @ /api/auth/send-verify-otp
+authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
+// @ /api/auth/verify-account
+authRouter.post("/verify-account", userAuth, verifyEmail);
 
 export default authRouter;
