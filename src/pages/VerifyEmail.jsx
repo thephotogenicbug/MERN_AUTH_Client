966ad29ab31d3fdc1c5ffac989c183142657dc9e
thephotogenicbug <navinkumarachari@gmail.com>
2025-06-26 16:25:17 +0530

    create function to handle otp and focus to next input & prev field and paste from clipboard

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -3,6 +3,33 @@ import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
 
 const VerifyEmail = () => {
+  const inputRefs = React.useRef([]);
+
+  // move focus to next input field on enter
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  // move focus to prev input field on delete
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  // handle paste from clipboard feature
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pasteArray = paste.split("");
+    pasteArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    });
+  };
+
   return (
     <div className=" flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
       <Link to="/">
@@ -19,7 +46,7 @@ const VerifyEmail = () => {
         <p className="text-center mb-6 text-indigo-400">
           Enter the 6-digit code sent to your email id.
         </p>
-        <div className="flex justify-between mb-8">
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
           {Array(6)
             .fill(0)
             .map((_, index) => (
@@ -29,6 +56,9 @@ const VerifyEmail = () => {
                 key={index}
                 required
                 className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
               />
             ))}
         </div>
